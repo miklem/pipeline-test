91ffc80469f8920b1f39f889e46e7c6057c0e4bc
Michael <miklem@users.noreply.github.com>
2024-02-03 16:29:07 +0100

    Merge pull request #9
    * Part of the feature ready to use
    
    * Add more functionality to the feature
    
    * Finished functionality to the feature
    
    * merge conflict fix

--- test_file.py
+++ test_file.py
@@ -5,4 +5,5 @@ print("this is print from f1 branch.")
 print("test file in f2 branch")
 
 print("some part of the feature from f3 branch")
-
+print("another part of the feature from f3 branch")
+print("the rest of the feature from f3 branch")
