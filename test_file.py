7b7e81630ce2d27a085dd80d4d444ddf1cde6e5d
mikle <miklem3d@gmail.com>
2024-02-03 18:45:45 +0100

    Squashed commit of the following:
    commit 73a6bc78977321ba73b177454d8b7095175956f4
    Author: mikle <miklem3d@gmail.com>
    Date:   Sat Feb 3 18:41:58 2024 +0100
    
        another changes in f4
    
    commit 918c03dfaedd462b01cfad4a9250b83c32e66875
    Author: mikle <miklem3d@gmail.com>
    Date:   Sat Feb 3 18:22:51 2024 +0100
    
        F4 rebased to dev
    
    # Conflicts:
    #	test_file.py

--- test_file.py
+++ test_file.py
@@ -7,3 +7,8 @@ print("test file in f2 branch")
 print("some part of the feature from f3 branch")
 print("another part of the feature from f3 branch")
 print("the rest of the feature from f3 branch")
+
+
+print("Changes from f4 branch")
+
+print("Another changes from f4 branch")
