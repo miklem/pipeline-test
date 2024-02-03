5e3193992ded2d8fc89e38c3ac4a25ca22f7079b
mikle <miklem3d@gmail.com>
2024-02-03 15:50:18 +0100

    Add more functionality to the feature
    Finished functionality to the feature
    
    Squashed commit of the following:
    
    commit 73a6bc78977321ba73b177454d8b7095175956f4
    Author: mikle <miklem3d@gmail.com>
    Date:   Sat Feb 3 18:41:58 2024 +0100
    
        another changes in f4
    
    commit 918c03dfaedd462b01cfad4a9250b83c32e66875
    Author: mikle <miklem3d@gmail.com>
    Date:   Sat Feb 3 18:22:51 2024 +0100
    
        F4 rebased to dev
    
    # Conflicts:
    #	test_file.py
    
    Squashed commit from F2 of the following:
    
    commit 253388453b24c5f49216076c0704ce814ee2a6a8
    Author: mikle <miklem3d@gmail.com>
    Date:   Sat Feb 3 18:48:11 2024 +0100
    
        changes in f2
    
    commit 97bf72f5ef6b1ae7f65506d31b6eda60e26f9f9a
    Author: mikle <miklem3d@gmail.com>
    Date:   Sat Feb 3 12:56:43 2024 +0100
    
        Modify gitignore
    
    commit 91ea2b3650f30cbc0de71edaed5af114639b1107
    Author: mikle <miklem3d@gmail.com>
    Date:   Fri Feb 2 19:43:13 2024 +0100
    
        Cherry picked from  99f7515
    
        Add gitignore for pycharm
    
    commit f8fdc05ddff610049998f8fd322e086f8bc75dda
    Author: mikle <miklem3d@gmail.com>
    Date:   Fri Feb 2 19:44:50 2024 +0100
    
        Add test file with print
    
    # Conflicts:
    #	test_file.py
    
    Squashed commit of the following:
    
    commit 73a6bc78977321ba73b177454d8b7095175956f4
    Author: mikle <miklem3d@gmail.com>
    Date:   Sat Feb 3 18:41:58 2024 +0100
    
        another changes in f4
    
    commit 918c03dfaedd462b01cfad4a9250b83c32e66875
    Author: mikle <miklem3d@gmail.com>
    Date:   Sat Feb 3 18:22:51 2024 +0100
    
        F4 rebased to dev
    
    Add more functionality to the feature
    
    squashing

--- test_file.py
+++ test_file.py
@@ -5,4 +5,10 @@ print("this is print from f1 branch.")
 print("test file in f2 branch")
 
 print("some part of the feature from f3 branch")
+print("another part of the feature from f3 branch")
+print("the rest of the feature from f3 branch")
 
+
+print("Changes from f4 branch")
+
+print("Another changes from f4 branch")
